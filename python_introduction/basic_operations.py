379e42468543b95218964101726220e2290dd790
sultan3141 <Adinanyous1@gmail.com>
2025-07-08 23:30:30 +0300

    Initial commit: add basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1=int(10)
+number2=int(5)
+addition=number1+number2
+subtraction=number1-number2
+multiplication=number1*number2
+print(f"addition of {number1} and {number2} is {addition}")
+print(f"subtraction of {number1} and {number2} is {subtraction}")
+print(f"multiplication of {number1} and {number2} is {multiplication}")
+
