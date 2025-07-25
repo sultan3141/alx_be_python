cc3a57bfa3ea807c8c2467573185ba50d756eda0
Sultan Adinan <Adinanyous1@gmail.com>
2025-07-25 23:32:39 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,13 +2,15 @@ def perform_operation(num1: float,num2: float ,operation: str):
  if operation=="add":
     return num1 + num2
 
-elif operation=="subtract":
+ elif operation=="subtract":
     return num1 - num2
 
-elif operation=="multiply":
+ elif operation=="multiply":
     return num1 * num2
 
-else operation=="divide":
+ elif operation=="divide":
     if b == 0:
         return "Cannot divide by zero"
     return num1 / num2
+ else:
+        return "Invalid operation"
