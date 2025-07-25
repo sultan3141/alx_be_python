fcab76b74c00978cbfc5f4e5ef0abfd9a4750de4
Sultan Adinan <Adinanyous1@gmail.com>
2025-07-26 00:17:02 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
     return num1 * num2
 
  elif operation=="divide":
-    if b == 0:
+    if num2 == 0:
         return "Cannot divide by zero"
     return num1 / num2
  else:
