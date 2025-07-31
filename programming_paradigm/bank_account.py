608c16c50de57bdb4c60342f59e615d2ea4cd15c
Sultan Adinan <Adinanyous1@gmail.com>
2025-07-31 23:59:17 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,1 +1,16 @@
+class BankAccount:
+    def __init__(self,account_balance):
+        self.__account_balance =account_balance
 
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+        else:
+            print("Insufficient funds")
+
+    def get_balance(self):
+        return self.__account_balance
