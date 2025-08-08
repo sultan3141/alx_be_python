494f0c4481710989d8dfe480fb8b20af62d94592
Sultan Adinan <Adinanyous1@gmail.com>
2025-08-08 23:50:13 +0300

    Create library_system.py

---
+++ oop/library_system.py
@@ -0,0 +1,19 @@
+class Book:
+  def __init__(self, title, author):
+    self.title=str(title)
+    self.author=str(author)
+  def __str__(self):
+     return f"Book: {self.title} and Prejudice by {self.author}"
+class EBook(Book):
+  def __init__(self,title, author,file_size):
+    super().__init__(title, author) 
+    self.file_size=int(file_size)
+  def __str__(self):
+    return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+class PrintBook(Book):
+   def __init__(self,title, author,page_count):
+     super().__init__(title, author) 
+     self.page_count=int(page_count)
+   def __str__(self):
+     return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+     
